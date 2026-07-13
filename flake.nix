27ed118e02cde3be7f10dcac3e3537144f3660f8
Raymond Baker <raymondgarethbaker@gmail.com>
2026-07-13 12:06:56 -0600

    Add flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+{
+  description = "Hello, Flakes!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        # jailbreakUnbreak = pkg:
+        #   pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
+        packageName = "hackathon";
+      in {
+        packages.${packageName} =
+          haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
+          };
+
+        packages.default = self.packages.${system}.${packageName};
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; (with haskellPackages; [
+            cabal-install
+          ]) ++ [
+          ];
+          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
+        };
+        devShell = self.devShells.${system}.default;
+        
+        shellHook = "PS1=\"[morphsimz.github.io:\\w]$ \"";
+      });
+}
